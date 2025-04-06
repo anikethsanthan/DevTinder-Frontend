9961188138c92f9f6a94a6e77059abfba8a734b9
anikethsanthan <anikethsanthan999@gmail.com>
2025-04-06 18:20:25 +0530

    Chat feature

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,4 +1,55 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { createSocketConnection } from "../utils/socket";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+
 const Chat = () => {
+  const { tagetUserId } = useParams();
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const userId = useSelector((store) => store?.user?._id);
+
+  const fetchChatMessages = async () => {
+    const chat = await axios.get(BASE_URL + `/chat/${tagetUserId}`, {
+      withCredentials: true,
+    });
+    console.log(chat.data?.chat?.messages);
+    setMessages(chat.data?.chat?.messages);
+  };
+  useEffect(() => {
+    fetchChatMessages();
+  }, []);
+
+  useEffect(() => {
+    if (!userId) return;
+    const socket = createSocketConnection();
+    socket.emit("joinChat", {
+      userId,
+      tagetUserId,
+    });
+
+    socket.on("receiveMessage", ({ userId, message }) => {
+      console.log("Received message:", message);
+      setMessages((messages) => [...messages, { userId, message }]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [userId, tagetUserId]);
+
+  const sendMessage = () => {
+    const socket = createSocketConnection();
+    socket.emit("sendMessage", {
+      userId,
+      tagetUserId,
+      message: newMessage,
+    });
+    setNewMessage("");
+  };
+
   return (
     <div className=" flex justify-center mt-[30px]">
       <div className="card glass w-[60%] h-[80%]">
@@ -8,50 +59,40 @@ const Chat = () => {
           <div className="border-b-2"></div>
           {/* Chat Section */}
 
-          {/* Receivers Chat */}
-          <div className="chat chat-start">
-            <div className="chat-image avatar">
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS chat bubble component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={`chat ${
+                msg.senderId._id === userId ? "chat-end" : "chat-start"
+              }`}
+            >
+              <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS chat bubble component"
+                    src={msg.senderId?.photoUrl}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="chat-header">
-              Obi-Wan Kenobi
-              <time className="text-xs opacity-50 ml-2">12:45</time>
-            </div>
-            <div className="chat-bubble">You were the Chosen One!</div>
-            <div className="chat-footer opacity-50">Delivered</div>
-          </div>
-
-          {/* Senders Chat */}
-          <div className="chat chat-end">
-            <div className="chat-image avatar">
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS chat bubble component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
+              <div className="chat-header">
+                {msg.senderId.firstName} {msg.senderId.lastName}
+                <time className="text-xs opacity-50 ml-2">12:45</time>
               </div>
+              <div className="chat-bubble">{msg.text}</div>
+              {/* <div className="chat-footer opacity-50">Delivered</div> */}
             </div>
-            <div className="chat-header">
-              Anakin
-              <time className="text-xs opacity-50 ml-2">12:46</time>
-            </div>
-            <div className="chat-bubble">I hate you!</div>
-            <div className="chat-footer opacity-50">Seen at 12:46</div>
-          </div>
+          ))}
 
           {/* Button Section */}
           <div className="card-actions justify-center border-t-2">
             <input
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
               type="text"
               placeholder="Type here"
               className="input input-bordered w-[80%] mt-4"
             />
-            <button className="btn  mt-4  h-[10px]">
+            <button onClick={sendMessage} className="btn  mt-4  h-[10px]">
               <i className="fa-solid fa-paper-plane"></i>
             </button>
           </div>
